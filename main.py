7e6e83104296c79f3e89c7e7e89f7056f173ac43
exotic_pig <rhtsudhakaran@gmail.com>
2025-04-26 12:30:34 -0400

    Add main.py

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def clean_sentence(sentence):
+    return sentence.split("+++$+++")[4].strip(" ").replace("\n", "")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+sentences = []
+
+with open("movie_lines.txt") as f:
+  for i in f.readlines():
+      sentences.append(clean_sentence(i))
+
+
+#
+# nltk.download('punkt_tab')  # For tokenizing (splitting text into words)
+#
+# nltk.download('punkt')  # For tokenizing (splitting text into words)
+#
+# trainer = PunktTrainer()
+# trainer.train(sentences)
+# trainer.finalize_training()
+# tokenizer = PunktSentenceTokenizer(trainer.get_params())
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(sentences)
+
+def chatbot_response(user_input):
+    user_vec = vectorizer.transform([user_input])
+    similarities = cosine_similarity(user_vec, X)
+    best_match = similarities.argmax()
+    return sentences[best_match]
+
+# Simulate chat
+while True:
+    user = input("You: ")
+    if user.lower() == 'bye':
+        print("Bot: Goodbye!")
+        break
+    response = chatbot_response(user)
+    print(f"Bot: {response}")
+
+
+
+
